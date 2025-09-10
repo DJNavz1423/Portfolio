daa09603c66681530f8cf7b030307aa217eb8b0b
djnavz1423 <danieljosh_navarro@sjp2cd.edu.ph>
2025-09-10 13:02:10 +0800

    Commit

--- PORT/JS/typewriter.js
+++ PORT/JS/typewriter.js
@@ -2,5 +2,5 @@ var typed = new Typed(".typing-text",{
   strings : ["Game Developer", "Web Developer", "Software Developer", "Penetration Tester"],
   typeSpeed : 150,
   backSpeed : 150,
-  looped : true 
+  loop : true 
 })
